bc42d0f3b40002ecbbc84c53f27d18f611d7c54c
Matthew Miller <matthew.miller@asmbly.org>
2024-04-11 16:00:30 -0500

    feat: deployment updates

--- dash_data_dashboard/entrypoint.sh
+++ dash_data_dashboard/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start the celery worker
-celery -A main.celery_app worker --loglevel=info &
+celery -A main.celery_app worker --loglevel=info -P solo &
 
 # Start the gunicorn server (production) or debug server (development)
 python -m main
